2a7ab644089dc56780f56c2eb2649956cd6fc4f7
tonyshh <shalin.anton8@gmail.com>
2023-10-11 15:53:10 +0700

    create base bot for testing (2.0 -> 3.0)

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-from aiogram import Bot, Dispatcher
-from config import API_TOKEN
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+from config import token
 
-
-bot = Bot(token=API_TOKEN, parse_mode="HTML")
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token)
+# Диспетчер
 dp = Dispatcher()
 
-if __name__ == '__main__':
-    from bot import handlers
-    dp.start_polling(skip_updates=True)
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
