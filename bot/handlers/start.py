9287e5d8e55657a28bf5c7da34f762c702ce1751
tonyshh <shalin.anton8@gmail.com>
2023-10-13 14:52:24 +0700

    change tree and files

--- bot/handlers/start.py
+++
@@ -1,7 +0,0 @@
-from aiogram import types
-from aiogram.dispatcher import Command
-from main import dp
-
-@dp.message_handler(commands=['start'])
-async def bot_start(message: types.Message):
-    await message.answer(f"Привет, {message.from_user.first_name}!\nЯ - бот для заказов изделий из дерева.")
